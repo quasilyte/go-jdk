255d6fc5213364277ccfce930540d8e88960858a
Iskander Sharipov <quasilyte@gmail.com>
2020-02-11 22:35:52 +0100

    cmd/javap: add -format option
    Currently, there are 2 formats:
    - "raw" is default, prints javap-like disasm
    - "ir" is a new format that outputs generated IR
    
    Fixes #5
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- cmd/javap/main.go
+++ cmd/javap/main.go
@@ -6,22 +6,30 @@ import (
 	"log"
 	"os"
 
+	"github.com/quasilyte/GopherJRE/irgen"
 	"github.com/quasilyte/GopherJRE/javap"
 	"github.com/quasilyte/GopherJRE/jclass"
 )
 
 func main() {
+	var args arguments
+	flag.StringVar(&args.format, "format", "raw",
+		`output format: raw or ir`)
 	flag.Parse()
 
 	filenames := flag.Args()
 	for _, filename := range filenames {
-		if err := printFile(filename); err != nil {
+		if err := printFile(&args, filename); err != nil {
 			log.Printf("%s: %v", filename, err)
 		}
 	}
 }
 
-func printFile(filename string) error {
+type arguments struct {
+	format string
+}
+
+func printFile(args *arguments, filename string) error {
 	f, err := os.Open(filename)
 	if err != nil {
 		return err
@@ -34,6 +42,34 @@ func printFile(filename string) error {
 		return fmt.Errorf("decode error: %v", err)
 	}
 
-	javap.Fprint(os.Stdout, jf)
+	switch args.format {
+	case "raw":
+		javap.Fprint(os.Stdout, jf)
+		return nil
+	case "ir":
+		return printFileIR(jf)
+	default:
+		return fmt.Errorf("unknown format: %s", args.format)
+	}
+}
+
+func printFileIR(jf *jclass.File) error {
+	c, err := irgen.ConvertClass(jf)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("name=%q pkg=%q\n", c.Name, c.PkgName)
+	for _, m := range c.Methods {
+		fmt.Printf("  method=%q\n", m.Name)
+		blockIndex := -1
+		for i, inst := range m.Code {
+			if inst.Flags.IsBlockLead() {
+				blockIndex++
+			}
+			fmt.Printf("        b%d %3d: %s\n", blockIndex, i, inst)
+		}
+	}
+
 	return nil
 }
