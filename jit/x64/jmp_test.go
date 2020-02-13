a9055ec0287922cae41bc74b1baa285cf29b297e
Iskander Sharipov <quasilyte@gmail.com>
2020-02-13 12:53:26 +0100

    jit/x64: add longer (32-bit) jump test
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ jit/x64/jmp_test.go
@@ -0,0 +1,23 @@
+package x64
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLongJumps(t *testing.T) {
+	checkEncoding := func(t *testing.T, result []byte, want string) {
+		have := fmt.Sprintf("%x", result)[:len(want)]
+		if have != want {
+			t.Errorf("encoding mismatch:\nhave: %s\nwant: %s", have, want)
+		}
+	}
+
+	t.Run("jmp", func(t *testing.T) {
+		asm := NewAssembler()
+		asm.Jmp(0)
+		asm.Nop(0x9988)
+		asm.Label(0)
+		checkEncoding(t, asm.Link(), "e988990000")
+	})
+}
