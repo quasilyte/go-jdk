71e128a5c1ed863343b796a4d8b24c3fb220625b
Iskander Sharipov <quasilyte@gmail.com>
2020-02-14 21:48:09 +0100

    cmd/internal/cmdutil: initial commit
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ cmd/internal/cmdutil/cmdutil.go
@@ -0,0 +1,17 @@
+package cmdutil
+
+import (
+	"os"
+
+	"github.com/quasilyte/GopherJRE/jclass"
+)
+
+func DecodeClassFile(filename string) (*jclass.File, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var dec jclass.Decoder
+	return dec.Decode(f)
+}
