b47ce4b11385fd9508a40f7d5194ba72c16bbdf0
Iskander Sharipov <quasilyte@gmail.com>
2020-02-17 22:23:43 +0100

    jdeps: initial commit
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ jdeps/jdeps.go
@@ -0,0 +1,11 @@
+package jdeps
+
+import (
+	"github.com/quasilyte/GopherJRE/jclass"
+)
+
+func ClassDependencies(c *jclass.File) []string {
+	// FIXME: analyze dependencies of a class file.
+	// See #23.
+	return nil
+}
