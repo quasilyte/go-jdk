112a5886d781a70f5fc23faaf642c21d5f8e129e
Iskander Sharipov <quasilyte@gmail.com>
2020-02-21 13:16:00 +0100

    jruntime: make jcall faster
    Call overhead: 11ns -> 4ns
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- jruntime/call_amd64.s
+++ jruntime/call_amd64.s
@@ -1,27 +1,32 @@
 #include "textflag.h"
+#include "funcdata.h"
 
-// func jcall(e *Env, code *byte)
+// Go asm has no real rip-based addressing, this is why we
+// encode `lea rax, [rip+N]` instruction manually, byte-by-byte.
+// N is the length of all instructions in this macro block.
+#define JCALL(fnreg) \
+        BYTE $0x48; BYTE $0x8d; BYTE $0x05; BYTE $0x09; BYTE $0; BYTE $0; BYTE $0; \
+        MOVQ AX, (SI) \
+        ADDQ $8, SI \
+        JMP fnreg
+
+// $96 bytes for Go call arguments space.
+// $16 bytes for 2 pointer arguments.
 //
 // Register roles:
 // AX - tmp register; also used for return value
 // BX - <unused> (pin to "this" pointer?)
 // CX - <unused>
-// DX - env pointer
+// DX - env pointer (no need to spill, always in an argument slot)
 // SI - stack pointer
 // DI - <unused>
-TEXT ·jcall(SB), 0, $8-16
+//
+// func jcall(e *Env, code *byte)
+TEXT ·jcall(SB), 0, $96-16
+        NO_LOCAL_POINTERS // TODO: Can we do without it?
         MOVQ e+0(FP), DX // env is always at DX
         MOVQ 1*8(DX), SI // stack is always at SI
         MOVQ code+8(FP), CX
-        CALL callwrapper(SB)
-        ADDQ $8, SP     // Because callwrapper has no RET
+        JCALL(CX)
         MOVQ AX, -8(SI) // Save called function result, if any
         RET
-
-// callwrapper captures return address written by a CALL
-// and jumps into CX.
-TEXT callwrapper(SB), 0, $0-0
-        MOVQ (SP), AX
-        MOVQ AX, (SI)
-        ADDQ $8, SI
-        JMP CX
