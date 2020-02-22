243b4dd3d85ccfe12cf8784661d4353869a3432b
Iskander Sharipov <quasilyte@gmail.com>
2020-02-22 05:47:01 +0100

    jdeps: find packages instead of class names
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- jdeps/jdeps.go
+++ jdeps/jdeps.go
@@ -1,23 +1,20 @@
 package jdeps
 
 import (
+	"strings"
+
 	"github.com/quasilyte/go-jdk/jclass"
 )
 
 // ClassDependencies returns a list of package names that given class depends on.
 // The list is not sorted.
 func ClassDependencies(c *jclass.File) []string {
-	return nil
-}
-
-func classDependencies(c *jclass.File) []string {
-	// FIXME: should return package names instead of type names.
-
-	visitor := makeVisitor(c)
+	visitor := depsFinder{map[string]struct{}{}, c}
 	visitor.addClass(c.SuperClass)
 	visitor.addClasses(c.Interfaces)
-	visitor.addFields()
-	visitor.addMethods()
+	visitor.walkConsts()
+	visitor.walkFields()
+	visitor.walkMethods()
 	// TODO: deal with inner classes
 	return visitor.keys()
 }
@@ -25,10 +22,6 @@ func classDependencies(c *jclass.File) []string {
 type depsFinder struct {
 	deps      map[string]struct{}
 	classFile *jclass.File
-}
-
-func makeVisitor(c *jclass.File) depsFinder {
-	return depsFinder{map[string]struct{}{}, c}
 }
 
 func (f *depsFinder) keys() []string {
@@ -39,8 +32,21 @@ func (f *depsFinder) keys() []string {
 	return keys
 }
 
+func (f *depsFinder) walkConsts() {
+	for _, c := range f.classFile.Consts {
+		switch c := c.(type) {
+		case *jclass.MethodrefConst:
+			f.addDependency(c.ClassName)
+			f.walkMethodDescriptor(c.Descriptor)
+		}
+	}
+}
+
 func (f *depsFinder) addDependency(d string) {
-	f.deps[d] = struct{}{}
+	i := strings.LastIndexByte(d, '/')
+	if i != -1 {
+		f.deps[d[:i]] = struct{}{}
+	}
 }
 
 func (f *depsFinder) addClass(i uint16) {
@@ -63,7 +69,7 @@ func (f *depsFinder) addType(typ jclass.DescriptorType) {
 	}
 }
 
-func (f *depsFinder) addFields() {
+func (f *depsFinder) walkFields() {
 	for _, field := range f.classFile.Fields {
 		// TODO: walk attributes
 		desc := jclass.FieldDescriptor(field.Descriptor)
@@ -72,14 +78,17 @@ func (f *depsFinder) addFields() {
 	}
 }
 
-func (f *depsFinder) addMethods() {
+func (f *depsFinder) walkMethods() {
 	for _, m := range f.classFile.Methods {
-		desc := jclass.MethodDescriptor(m.Descriptor)
 		// TODO: walk attributes
-
-		desc.WalkParams(func(typ jclass.DescriptorType) {
-			f.addType(typ)
-		})
-		f.addType(desc.ReturnType())
+		f.walkMethodDescriptor(m.Descriptor)
 	}
+}
+
+func (f *depsFinder) walkMethodDescriptor(s string) {
+	desc := jclass.MethodDescriptor(s)
+	desc.WalkParams(func(typ jclass.DescriptorType) {
+		f.addType(typ)
+	})
+	f.addType(desc.ReturnType())
 }
