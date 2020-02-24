7cc624852c5a8626bcc10388c79cc12226559700
Iskander Sharipov <quasilyte@gmail.com>
2020-02-24 13:50:46 +0100

    iropt: initial commit
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ iropt/iropt.go
@@ -0,0 +1,11 @@
+package iropt
+
+import (
+	"github.com/quasilyte/go-jdk/ir"
+	"github.com/quasilyte/go-jdk/vmdat"
+)
+
+func Optimize(st *vmdat.State, packages []*ir.Package) {
+	// Does nothing right now.
+	// Stub for the future.
+}
