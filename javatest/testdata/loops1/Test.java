6f2579a9c2fc7e73baf1cef0ef3da8624d01544a
Iskander Sharipov <quasilyte@gmail.com>
2020-02-27 18:31:10 +0300

    javatest: uncomment tests
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- javatest/testdata/loops1/Test.java
+++ javatest/testdata/loops1/Test.java
@@ -4,23 +4,27 @@ import testutil.T;
 
 public class Test {
     public static void run(int x) {
-        // TODO: uncomment code below when it stops crashing the runtime.
+        int v = 0;
+        for (int i = 0; i < 100; i++) {
+            if (i < 50) {
+                v++;
+            }
+        }
+        T.printInt(v);
 
-        // int v = 0;
-        // for (int i = 0; i < 100; i++) {
-        //     if (i < 50) {
-        //         v++;
-        //     }
-        // }
-        // T.printInt(v);
+        v = 101;
+        int i = 0;
+        while (i < 10) {
+            T.printInt(i);
+            v -= i;
+            i += 3;
+        }
+        T.printInt(v);
 
-        // int v = 101;
-        // int i = 0;
-        // while (i < 10) {
-        //     T.printInt(i);
-        //     v -= i;
-        //     i += 3;
-        // }
-        // T.printInt(v);
+        i = 110;
+        do {
+            i--;
+        } while (i >= 100);
+        T.printInt(i);
     }
 }
--- javatest/testdata/scopes/Test.java
+++ javatest/testdata/scopes/Test.java
@@ -4,20 +4,20 @@ import testutil.T;
 
 public class Test {
     public static void run(int x) {
-        // T.printInt(x);
-        // {
-        //     int x2 = 10;
-        //     T.printInt(x2);
-        // }
-        // T.printInt(x);
-        // x = 20;
-        // {
-        //     int x2 = 30;
-        //     int y = 20;
-        //     T.printInt(y);
-        // }
-        // int y = 30;
-        // T.printInt(y);
-        // T.printInt(x);
+        T.printInt(x);
+        {
+            int x2 = 10;
+            T.printInt(x2);
+        }
+        T.printInt(x);
+        x = 20;
+        {
+            int x2 = 30;
+            int y = 20;
+            T.printInt(y);
+        }
+        int y = 30;
+        T.printInt(y);
+        T.printInt(x);
     }
 }
